ec6dbe976cf2ac40ed90eb2f1a1d3c8ae6dff551
arenasjuanf <juanfa107@gmail.com>
2022-02-19 16:07:52 -0500

    creación de lexemas y tokens

--- src/caracteres.ts
+++ src/caracteres.ts
@@ -1,13 +1,76 @@
+interface caracter {
+    token: string,
+    lexema: string,
+}
+
 export default class caracteres{
 
-    private caracteres = [
+    private caracteres:caracter[] = [
         {
-            token: 'pro',
-            lexema: 'programa',
-            
-        }
+            token: 'funct',
+            lexema: 'function',
+        },
+        {
+            token: "=",
+            lexema: "Equal_To"
+        },
+        {
+            token: "(",
+            lexema: "("
+        },
+        {
+            token: "(",
+            lexema: ")"
+        },
+        {
+            token: "+",
+            lexema: "Add"
+        },
+        {
+            token: "-",
+            lexema: "substract"
+        },
+        {
+            token: "/",
+            lexema: "Divide"
+        },
+        {
+            token: "*",
+            lexema: "multiply"
+        },
+        {
+            token: "return",
+            lexema: "return"
+        },
+        {
+            token: "if",
+            lexema: "COND_IF"
+        },
+        {
+            token: "else",
+            lexema: "COND_ELSE"
+        },
+        {
+            token: "==",
+            lexema: "COND_EQUAL"
+        },
+        {
+            token: ">",
+            lexema: "COND_>"
+        },
+        {
+            token: "<",
+            lexema: "COND_<"
+        },
+        {
+            token: "=",
+            lexema: "="
+        },
+
     ]
 
-
+    getCaracteres(){
+        return this.caracteres;
+    }
 
 }
