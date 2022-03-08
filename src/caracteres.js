c7dc8dd7608b89de7ff81184f8a8cdbc4bfa8113
arenasjuanf <juanfa107@gmail.com>
2022-03-07 22:19:57 -0500

    add aritmetic symbols expressions and catch error

--- src/caracteres.js
+++ src/caracteres.js
@@ -9,57 +9,90 @@ class Analyzer {
     constructor() {
         this.lexer = new tokenizr_1.default();
         this.initLexer();
+        this.lexer.error("********************************");
     }
     initLexer() {
+        //Function
+        this.lexer.rule(/(fun [a-zA-Z])\w+\(.*\)/, (ctx, [match]) => {
+            const functName = match.split('(')[0].replace('fun ', "");
+            let [params] = match.match(/\(([^()]*)\)/);
+            params = params.replace('(', '').replace(')', '').split(',');
+            ctx.accept("Function", `${functName}`);
+            params.forEach((param) => {
+                ctx.accept(`Argument ${functName}`, `${param.trim()}`);
+            });
+        });
+        //Id
         this.lexer.rule(/[a-zA-Z_][a-zA-Z0-9_]*/, (ctx, match) => {
-            ctx.accept("id");
+            ctx.accept("Id");
         });
+        //Number
         this.lexer.rule(/[+-]?[0-9]+/, (ctx, match) => {
-            ctx.accept("number", parseInt(match[0]));
+            ctx.accept("Number", parseInt(match[0]));
         });
+        //String
         this.lexer.rule(/"((?:\\"|[^\r\n])*)"/, (ctx, match) => {
-            ctx.accept("string", match[1].replace(/\\"/g, "\""));
+            ctx.accept("String", match[1].replace(/\\"/g, "\""));
         });
+        //Comment 1 line
         this.lexer.rule(/\/\/[^\r\n]*\r?\n/, (ctx, [match]) => {
-            ctx.accept("comment", match.replace('//', ""));
+            ctx.accept("Comment", match.replace('//', ""));
         });
+        //Blanks
         this.lexer.rule(/[ \t\r\n]+/, (ctx, _) => {
             ctx.ignore(); // ignore spaces
         });
+        //Blocks
         this.lexer.rule(/[{}]/, (ctx, [match]) => {
             const types = {
-                '{': 'OPEN',
-                '}': 'CLOSE'
+                '{': 'Open',
+                '}': 'Close'
             };
             ctx.accept(`${types[match]} block`);
         });
-        this.lexer.rule(/[()]/, (ctx, [match]) => {
+        // Parenthesis
+        this.lexer.rule(/[()]/, (ctx, x) => {
+            const match = x[0];
             const types = {
-                '(': 'OPEN',
-                ')': 'CLOSE'
+                '(': 'Open',
+                ')': 'Close'
             };
             ctx.accept(`${types[match]} parenthesis`);
         });
-        this.lexer.rule(/./, (ctx, _) => {
-            ctx.accept("char");
+        //Character
+        this.lexer.rule(/( ?[-+*\/=])/, (ctx, [match]) => {
+            const characters = {
+                '=': 'Equal',
+                '+': 'Add',
+                '-': 'Minus',
+                '*': 'Multiply',
+                '/': 'Divide'
+            };
+            //
+            ctx.accept(`Character`, characters[match]);
         });
     }
     analyze(value, debug = false) {
-        this.lexer.input(value);
-        this.lexer.debug(debug);
-        const lines = {};
-        this.lexer.tokens().forEach((token, pos) => {
-            const { line } = token;
-            lines[line] || (lines[line] = []);
-            lines[line].push(token);
-        });
-        Object.keys(lines).forEach((line) => {
-            console.log(`LINE ${line} : `);
-            lines[line].forEach(({ type, value, column }) => {
-                console.log(` → Type: ${chalk_1.default.red(type)} , Value: ${chalk_1.default.magentaBright(value)} , Column: ${chalk_1.default.green(column)}`);
+        try {
+            this.lexer.input(value);
+            this.lexer.debug(debug);
+            const lines = {};
+            this.lexer.tokens().forEach((token, pos) => {
+                const { line } = token;
+                lines[line] || (lines[line] = []);
+                lines[line].push(token);
             });
-            console.log(chalk_1.default.cyan('-----------------------------------------------------------------------------------------------'));
-        });
+            Object.keys(lines).forEach((line) => {
+                console.log(chalk_1.default.greenBright(`LINE ${line} : `));
+                lines[line].forEach(({ type, value, column }) => {
+                    console.log(` → Type: ${chalk_1.default.red(type)} , Value: ${chalk_1.default.magentaBright(value)} , Column: ${chalk_1.default.green(column)}`);
+                });
+                console.log(chalk_1.default.cyan('-----------------------------------------------------------------'));
+            });
+        }
+        catch ({ pos, line, column }) {
+            console.log(chalk_1.default.red(JSON.stringify({ msg: 'token not recognized', pos, line, column })));
+        }
     }
 }
 exports.default = Analyzer;
--- src/caracteres.ts
+++ src/caracteres.ts
@@ -7,67 +7,102 @@ export default class Analyzer{
 
     constructor(){
         this.initLexer();
+        this.lexer.error("********************************");
     }
 
     initLexer(){
 
+        //Function
+        this.lexer.rule(/(fun [a-zA-Z])\w+\(.*\)/, (ctx, [match]) => {
+            const functName = match.split('(')[0].replace('fun ',"");
+            let [params]= match.match(/\(([^()]*)\)/) as any [];
+            params = params.replace('(','').replace(')','').split(',');
+            ctx.accept("Function", `${functName}`);
+            params.forEach((param: string) => {
+                ctx.accept(`Argument ${functName}`, `${param.trim()}`);
+            });
+        });
+
+        //Id
         this.lexer.rule(/[a-zA-Z_][a-zA-Z0-9_]*/, (ctx, match) => {
-            ctx.accept("id")
-        })
+            ctx.accept("Id")
+        });
+
+        //Number
         this.lexer.rule(/[+-]?[0-9]+/, (ctx, match) => {
-            ctx.accept("number", parseInt(match[0]))
-        })
+            ctx.accept("Number", parseInt(match[0]))
+        });
+
+        //String
         this.lexer.rule(/"((?:\\"|[^\r\n])*)"/, (ctx, match) => {
-            ctx.accept("string", match[1].replace(/\\"/g, "\""))
+            ctx.accept("String", match[1].replace(/\\"/g, "\""))
         })
+
+        //Comment 1 line
         this.lexer.rule(/\/\/[^\r\n]*\r?\n/, (ctx, [match]) => {
-            ctx.accept("comment", match.replace('//', ""))
+            ctx.accept("Comment", match.replace('//', ""))
         })
+
+        //Blanks
         this.lexer.rule(/[ \t\r\n]+/, (ctx, _) => {
             ctx.ignore() // ignore spaces
         })
 
+        //Blocks
         this.lexer.rule(/[{}]/, (ctx, [match]) => {
             const types = {
-                '{': 'OPEN',
-                '}': 'CLOSE'
+                '{': 'Open',
+                '}': 'Close'
             }
             ctx.accept(`${types[match as ('{' | '}')]} block`)
         })
 
-        this.lexer.rule(/[()]/, (ctx, [match]) => {
+        // Parenthesis
+        this.lexer.rule(/[()]/, (ctx, x) => {
+            const match = x[0];
             const types = {
-                '(': 'OPEN',
-                ')': 'CLOSE'
+                '(': 'Open',
+                ')': 'Close'
             }
             ctx.accept(`${types[match as ('(' | ')')]} parenthesis`)
         })
 
+        //Character
+        this.lexer.rule(/( ?[-+*\/=])/, (ctx, [match]) => {
+            const characters = {
+                '=': 'Equal',
+                '+': 'Add',
+                '-': 'Minus',
+                '*': 'Multiply',
+                '/': 'Divide'
+            }
 
-        this.lexer.rule(/./, (ctx, _) => {
-            ctx.accept("char")
+            //
+            ctx.accept(`Character`, characters[match as ('='| '+' | '-' | '*' | '/') ]);
         })
+
     }
 
     analyze(value: string, debug: boolean  = false){
-        this.lexer.input(value);
-        this.lexer.debug(debug);
-        const lines: any = {};
-
-        this.lexer.tokens().forEach((token: Token, pos: number) => {
-            const {line} = token;
-            lines[line] ||= [];
-            lines[line].push(token);
-        })
-
-        Object.keys(lines).forEach((line) => {
-
-            console.log(`LINE ${line} : `);
-
-            (lines[line] as Token[]).forEach(({type, value, column}) => {
-                console.log(` → Type: ${chalk.red(type)} , Value: ${chalk.magentaBright(value)} , Column: ${chalk.green(column)}`);
+        try{
+            this.lexer.input(value);
+            this.lexer.debug(debug);
+            const lines: any = {};
+            this.lexer.tokens().forEach((token: Token, pos: number) => {
+                const {line} = token;
+                lines[line] ||= [];
+                lines[line].push(token);
             })
-            console.log(chalk.cyan('-----------------------------------------------------------------------------------------------'));
-        })
+    
+            Object.keys(lines).forEach((line) => {
+                console.log(chalk.greenBright(`LINE ${line} : `));
+                (lines[line] as Token[]).forEach(({type, value, column}) => {
+                    console.log(` → Type: ${chalk.red(type)} , Value: ${chalk.magentaBright(value)} , Column: ${chalk.green(column)}`);
+                })
+                console.log(chalk.cyan('-----------------------------------------------------------------'));
+            })
+        }catch({pos, line, column}){
+            console.log(chalk.red(JSON.stringify({msg: 'token not recognized', pos, line, column})));
+        }
     }
 }
